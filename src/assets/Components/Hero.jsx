3b378b712353bc00cc9d6640d2817d17f692be6c
unknown <ayazerguine.dev@gmail.com>
2025-11-12 16:15:48 +0100

    Added resume

--- src/assets/Components/Hero.jsx
+++ src/assets/Components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <main className='hero'>
         <h2>Hi, I’m <span className="highlight">Aya Zerguine</span> <br /> <span className="subtitle">Frontend Web Developer</span></h2>
         <p>Self-taught frontend developer passionate about clean design and smooth UX. I focus on turning ideas into responsive, maintainable interfaces using <strong>React</strong> and modern web tools.</p>
-        <button>Download Resume <i class="fa-solid fa-download"></i></button>
+        <a href="/Aya_Zerguine_Frontend_Developer_Resume.pdf" download><button>Download Resume <i class="fa-solid fa-download"></i></button></a>
       </main>
    )
 }
